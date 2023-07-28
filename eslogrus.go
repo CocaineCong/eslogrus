da9b49b91ff6c514a9049ea27626a9c785bd111b
FanOne <294350394@qq.com>
2023-07-29 01:44:34 +0800

    feat:新增一些注释

--- eslogrus.go
+++ eslogrus.go
@@ -16,15 +16,16 @@ import (
 // ElasticHook is a logrus
 // hook for ElasticSearch
 type ElasticHook struct {
-	client    *elastic.Client
-	host      string
-	index     IndexNameFunc
-	levels    []logrus.Level
-	ctx       context.Context
-	ctxCancel context.CancelFunc
-	fireFunc  fireFunc
+	client    *elastic.Client    // es的客户端
+	host      string             // es 的 host
+	index     IndexNameFunc      // 获取索引的名字
+	levels    []logrus.Level     // 日志的级别 info，error
+	ctx       context.Context    // 上下文
+	ctxCancel context.CancelFunc // 上下文cancel的函数，
+	fireFunc  fireFunc           // 需要实现这个
 }
 
+// 发送到es的信息结构
 type message struct {
 	Host      string
 	Timestamp string `json:"@timestamp"`
@@ -38,11 +39,7 @@ type IndexNameFunc func() string
 
 type fireFunc func(entry *logrus.Entry, hook *ElasticHook) error
 
-// NewElasticHook
-// client is an elastic client
-// host is the elastic's host
-// level is the level of logrus
-// index is the es index you wanna create message in. by the way , if the index don't exist, the esapi will create index automatically
+// NewElasticHook 新建一个es hook对象
 func NewElasticHook(client *elastic.Client, host string, level logrus.Level, index string) (*ElasticHook, error) {
 	return NewElasticHookWithFunc(client, host, level, func() string { return index })
 }
@@ -51,6 +48,7 @@ func NewElasticHookWithFunc(client *elastic.Client, host string, level logrus.Le
 	return newHookFuncAndFireFunc(client, host, level, indexFunc, syncFireFunc)
 }
 
+// 新建一个hook
 func newHookFuncAndFireFunc(client *elastic.Client, host string, level logrus.Level, indexFunc IndexNameFunc, fireFunc fireFunc) (*ElasticHook, error) {
 	var levels []logrus.Level
 	for _, l := range []logrus.Level{
@@ -79,6 +77,7 @@ func newHookFuncAndFireFunc(client *elastic.Client, host string, level logrus.Le
 	}, nil
 }
 
+// createMessage 创建信息
 func createMessage(entry *logrus.Entry, hook *ElasticHook) *message {
 	level := entry.Level.String()
 
@@ -97,6 +96,7 @@ func createMessage(entry *logrus.Entry, hook *ElasticHook) *message {
 	}
 }
 
+// syncFireFunc 异步发送
 func syncFireFunc(entry *logrus.Entry, hook *ElasticHook) error {
 	data, err := json.Marshal(createMessage(entry, hook))
 
@@ -120,11 +120,12 @@ func syncFireFunc(entry *logrus.Entry, hook *ElasticHook) error {
 	return err
 }
 
+// Fire 实现 logrus hook 必须要的接口函数
 func (hook *ElasticHook) Fire(entry *logrus.Entry) error {
 	return hook.fireFunc(entry, hook)
 }
 
-// Levels Required for logrus hook implementation
+// Levels 实现 logrus hook 必须要的接口函数
 func (hook *ElasticHook) Levels() []logrus.Level {
 	return hook.levels
 }
